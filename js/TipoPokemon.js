46959169ac07d0a59316c0215615774889ce3378
Pedro Martins <pedroldesmartins@gmail.com>
2025-05-25 21:18:03 -0300

    Add files via upload

---
+++ js/TipoPokemon.js
@@ -0,0 +1,23 @@
+export const TipoPokemon = Object.freeze
+(
+    {
+        NORMAL: 'Normal',
+        FOGO: 'Fogo',
+        AGUA: 'Agua',
+        GRAMA: 'Grama',
+        ELETRICO: 'Eletrico',
+        GELO: 'Gelo',
+        LUTADOR: 'Lutador',
+        VENENO: 'Veneno',
+        TERRA: 'Terra',
+        VOADOR: 'Voador',
+        PSICO: 'Psiquico',
+        INSETO: 'Inseto',
+        PEDRA: 'Pedra',
+        FANTASMA: 'Fantasma',
+        DRAGAO: 'Dragão',
+        ACO: 'Aço',
+        FADA: 'Fada',
+        SOMBRIO: 'Sombrio'
+    }
+);
